3fa2422aec98e00e9517017ea08c79b1cad29b83
uknow-mac <yoon6763@naver.com>
2024-03-07 16:12:09 +0900

    API 테스트로 전환하기

--- src/main/java/org/example/productorderservice/order/OrderService.java
+++ src/main/java/org/example/productorderservice/order/OrderService.java
@@ -1,9 +1,17 @@
 package org.example.productorderservice.order;
 
 import org.example.productorderservice.product.Product;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @Component
+@RestController
+@RequestMapping("/orders")
 public class OrderService {
 
     private final OrderPort orderPort;
@@ -12,9 +20,11 @@ public class OrderService {
         this.orderPort = orderPort;
     }
 
-    public void createOrder(CreateOrderRequest request) {
+    @PostMapping
+    public ResponseEntity<Void> createOrder(@RequestBody CreateOrderRequest request) {
         final Product product = orderPort.getProductById(request.productId());
         final Order order = new Order(product, request.quantity());
         orderPort.save(order);
+        return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 }
